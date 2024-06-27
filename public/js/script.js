99ac57cf22996d5ad80e4483e8700508d1b66185
Wenly Rusli <wenlyrusli7@gmail.com>
2024-06-27 14:55:38 +0700

    set mobil
    tinggal ganti sama yang real

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreLoader'));
 
-
-
+function changeImage(imageSrc) {
+    document.getElementById('featured-photo').src = imageSrc;
+    }
